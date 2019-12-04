0c76905406084ffea91b78133c47f1806849c439
Suhrit Duttagupta <suhritdgupta@gmail.com>
2019-12-04 20:10:17 +0100

    Wrote Readme

--- Temp.py
+++ Temp.py
@@ -6,13 +6,13 @@ Created on Thu Oct 17 11:11:25 2019
 """
 
 def F2C(temp):
-    print("Your converted temperature is ",str((temp-32)*5/9))
+    print("Your converted temperature is ",str((temp-32)*5/9)," C")
     
 def C2F(temp):
-    print("Your converted temperature is ", str(temp*1.8+32))
+    print("Your converted temperature is ", str(temp*1.8+32)," F")
 
 temp = input("Enter the temperature.")
-x = input("Enter the unit of temperature, C/F")
+x = input("Enter the unit of temperature, C/F ")
 if (x=='C' or x=='c'):
     C2F(float(temp))
 elif(x=='F' or x=='f'):
